576f4403e85f6699532aaa083f58b0af9263a992
Isatvikmishra <asksatvik@gmail.com>
2024-08-13 00:46:32 +0530

    slugs made, watchlist made though remove from watchlist still left

---
+++ src/store/watchlistSlice.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const watchlistSlice = createSlice({
+    name: "watchlistSlice",
+    initialState: [],
+    reducers: {
+        handleAddCoins(state, actions){
+                const newState = [...state]
+                newState.push(actions.payload);
+
+                return newState
+        },
+        handleRemoveCoins(state, actions){
+            console.log(state, actions); 
+        },
+    }
+})
+export const {handleAddCoins, handleRemoveCoins} = watchlistSlice.actions
+export default watchlistSlice.reducer;
